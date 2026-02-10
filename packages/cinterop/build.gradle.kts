a4487269924d39a505ca01e8c7c8d40121e00c05
Xilin Jia <6257601+XilinJia@users.noreply.github.com>
2026-02-10 18:38:57 +0100

    removed macos x86_64

--- packages/cinterop/build.gradle.kts
+++ packages/cinterop/build.gradle.kts
@@ -608,7 +608,7 @@ fun Task.buildSharedLibrariesForJVMMacOs() {
                 "cmake",
                 *cmakeFlags,
                 "-DCPACK_PACKAGE_DIRECTORY=..",
-                "-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64",
+                "-DCMAKE_OSX_ARCHITECTURES=arm64",
                 project.file("src/jvm/")
             )
         }
@@ -788,7 +788,7 @@ fun Task.build_C_API_Macos_Universal(buildVariant: BuildType) {
                 "-DCMAKE_SYSTEM_NAME=Darwin",
                 "-DCPACK_SYSTEM_NAME=macosx",
                 "-DCPACK_PACKAGE_DIRECTORY=..",
-                "-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64",
+                "-DCMAKE_OSX_ARCHITECTURES=arm64",
                 "-G",
                 "Xcode",
                 ".."
