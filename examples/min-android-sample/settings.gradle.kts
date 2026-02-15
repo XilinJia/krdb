d8c6d018ccbfd7e2101d45f7ca49d6d20e2f367d
Xilin Jia <6257601+XilinJia@users.noreply.github.com>
2026-02-15 15:45:50 +0000

    actions test

--- examples/min-android-sample/settings.gradle.kts
+++ examples/min-android-sample/settings.gradle.kts
@@ -1,3 +1,19 @@
 rootProject.name = "MinAndroidExample"
 include(":shared")
 include(":app")
+
+pluginManagement {
+    repositories {
+        google()            // <--- Add this first
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()            // <--- Also ensure it's here for dependencies
+        mavenCentral()
+    }
+}
