7216c9edbedb4146d26da44209ec69418c0cbca3
Xilin Jia <6257601+XilinJia@users.noreply.github.com>
2026-02-17 09:29:06 +0000

    3.3.2 release

--- tools/sync_test_server/bind_android_ports.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This scripts tries to bind the android emulator ports to point the local development server.
-
-adb reverse tcp:9443 tcp:9443 && \
-adb reverse tcp:9080 tcp:9080 && \
-adb reverse tcp:9090 tcp:9090 && \
-echo "Done" || { echo "Failed to reverse android emulator ports." ;}
